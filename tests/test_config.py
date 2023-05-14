8be1726070c92d1258b5381d4a104320b363ea89
a.kochanke <a.kochanke@posteo.net>
2023-05-14 10:48:53 +0200

    move fixtures to single file

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,18 @@
+from typing import Tuple
 import pathlib
 
+from omegaconf import DictConfig, OmegaConf
 import pytest
 
 
 @pytest.fixture
 def root_dir() -> pathlib.Path:
     return pathlib.Path(__file__).parents[1]
+
+
+@pytest.fixture
+def tparams_hparams(root_dir: pathlib.Path) -> Tuple[DictConfig, DictConfig]:
+    full_params = OmegaConf.load(root_dir / "aintelope/config/config_experiment.yaml")
+    tparams = full_params.trainer_params
+    hparams = full_params.hparams
+    return tparams, hparams
