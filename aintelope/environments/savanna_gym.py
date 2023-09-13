b9c61c159f6b89b769b07dcc6ac98b81bcb9e1e0
Roland Pihlakas <roland@simplify.ee>
2023-09-13 22:02:04 +0300

    One more change for Gymnasium compatibility.

--- aintelope/environments/savanna_gym.py
+++ aintelope/environments/savanna_gym.py
@@ -46,7 +46,14 @@ class SavannaGymEnv(SavannaEnv, gym.Env):
         # but per agent
         res = SavannaEnv.step(self, actions)
 
-        observations, rewards, dones, infos = res
+        if gym_v26:
+            observations, rewards, terminateds, truncateds, infos = res
+            dones = [
+                terminated or truncated
+                for (terminated, truncated) in zip(terminateds, truncateds)
+            ]
+        else:
+            observations, rewards, dones, infos = res
 
         # so just return the first
         i = self._agent_id
