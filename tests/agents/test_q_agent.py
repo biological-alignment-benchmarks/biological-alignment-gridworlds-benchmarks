938419b30fd30f2f1d0fe36c36a4f560413da53a
Roland Pihlakas <roland@simplify.ee>
2023-12-21 18:12:43 +0200

    Tests for parallel environments, tests for multi-agent environments death handling.

--- tests/agents/test_q_agent.py
+++ tests/agents/test_q_agent.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import pytest
+import numpy as np
+import time
 from typing import Tuple, Dict
 
 from omegaconf import OmegaConf, DictConfig
@@ -13,151 +15,232 @@ from tests.test_config import (
 from aintelope.training.simple_eval import run_episode
 
 
-def test_qagent_in_savanna_zoo_sequential(tparams_hparams: Dict) -> None:  # TODO
-    full_params = tparams_hparams
-    params_savanna_zoo = {
-        "agent_id": "q_agent",
-        "env": "savanna-zoo-sequential-v2",
-        "env_entry_point": "aintelope.environments.savanna_zoo:SavannaZooSequentialEnv",
-        "env_type": "zoo",
-        "sequential_env": True,
-        "env_params": {
-            "num_iters": 40,  # duration of the game
-            "map_min": 0,
-            "map_max": 20,
-            "render_map_max": 20,
-            "amount_agents": 1,  # for now only one agent
-            "amount_grass_patches": 2,
-            "amount_water_holes": 0,
-        },
-        "agent_params": {},
-    }
-    full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
-    run_episode(full_params=full_params)
+def test_qagent_in_savanna_zoo_sequential(tparams_hparams: Dict) -> None:
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        np.random.seed(index)
+
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-zoo-sequential-v2",
+            "env_entry_point": "aintelope.environments.savanna_zoo:SavannaZooSequentialEnv",
+            "env_type": "zoo",
+            "sequential_env": True,
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 1,  # for now only one agent
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
 
 
 def test_qagent_in_savanna_zoo_sequential_with_death(
     tparams_hparams: Dict,
-) -> None:  # TODO
-    full_params = tparams_hparams
-    params_savanna_zoo = {
-        "agent_id": "q_agent",
-        "env": "savanna-zoo-sequential-v2",
-        "env_entry_point": "aintelope.environments.savanna_zoo:SavannaZooSequentialEnv",
-        "env_type": "zoo",
-        "sequential_env": True,
-        "env_params": {
-            "num_iters": 40,  # duration of the game
-            "map_min": 0,
-            "map_max": 20,
-            "render_map_max": 20,
-            "amount_agents": 2,  # needed for death test
-            "amount_grass_patches": 2,
-            "amount_water_holes": 0,
-            "test_death": True,
-        },
-        "agent_params": {},
-    }
-    full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
-    run_episode(full_params=full_params)
+) -> None:
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        np.random.seed(index)
+
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-zoo-sequential-v2",
+            "env_entry_point": "aintelope.environments.savanna_zoo:SavannaZooSequentialEnv",
+            "env_type": "zoo",
+            "sequential_env": True,
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 2,  # needed for death test
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+                "test_death": True,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
 
 
 def test_qagent_in_savanna_gridworlds_sequential(tparams_hparams: Dict) -> None:
-    full_params = tparams_hparams
-    params_savanna_zoo = {
-        "agent_id": "q_agent",
-        "env": "savanna-safetygrid-sequential-v1",
-        "env_entry_point": "aintelope.environments.savanna_safetygrid:SavannaGridworldSequentialEnv",
-        "env_type": "zoo",
-        "env_params": {
-            "num_iters": 40,  # duration of the game
-            "map_min": 0,
-            "map_max": 20,
-            "render_map_max": 20,
-            "amount_agents": 1,  # for now only one agent
-            "amount_grass_patches": 2,
-            "amount_water_holes": 0,
-        },
-        "agent_params": {},
-    }
-    full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
-    run_episode(full_params=full_params)
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-safetygrid-sequential-v1",
+            "env_entry_point": "aintelope.environments.savanna_safetygrid:SavannaGridworldSequentialEnv",
+            "env_type": "zoo",
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 1,  # for now only one agent
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+                "seed": index,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
 
 
 def test_qagent_in_savanna_gridworlds_sequential_with_death(
     tparams_hparams: Dict,
 ) -> None:
-    full_params = tparams_hparams
-    params_savanna_zoo = {
-        "agent_id": "q_agent",
-        "env": "savanna-safetygrid-sequential-v1",
-        "env_entry_point": "aintelope.environments.savanna_safetygrid:SavannaGridworldSequentialEnv",
-        "env_type": "zoo",
-        "env_params": {
-            "num_iters": 40,  # duration of the game
-            "map_min": 0,
-            "map_max": 20,
-            "render_map_max": 20,
-            "amount_agents": 2,  # needed for death test
-            "amount_grass_patches": 2,
-            "amount_water_holes": 0,
-            "test_death": True,
-        },
-        "agent_params": {},
-    }
-    full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
-    run_episode(full_params=full_params)
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-safetygrid-sequential-v1",
+            "env_entry_point": "aintelope.environments.savanna_safetygrid:SavannaGridworldSequentialEnv",
+            "env_type": "zoo",
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 2,  # needed for death test
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+                "test_death": True,
+                "seed": index,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
 
 
-# TODO: parallel API support
+def test_qagent_in_savanna_zoo_parallel(tparams_hparams: Dict) -> None:
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        np.random.seed(index)
 
-# def test_qagent_in_savanna_zoo_parallel(  # TODO
-#    tparams_hparams: Dict
-# ) -> None:
-#    full_params = tparams_hparams
-#    params_savanna_zoo = {
-#        "agent_id": "q_agent",
-#        "env": "savanna-zoo-parallel-v2",
-#        "env_entry_point": "aintelope.environments.savanna_zoo:SavannaZooParallelEnv",
-#        "env_type": "zoo",
-#        "sequential_env": True,
-#        "env_params": {
-#            "num_iters": 40,  # duration of the game
-#            "map_min": 0,
-#            "map_max": 20,
-#            "render_map_max": 20,
-#            "amount_agents": 1,  # for now only one agent
-#            "amount_grass_patches": 2,
-#            "amount_water_holes": 0,
-#        },
-#        "agent_params": {},
-#    }
-#    full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
-#    run_episode(full_params=full_params)
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-zoo-parallel-v2",
+            "env_entry_point": "aintelope.environments.savanna_zoo:SavannaZooParallelEnv",
+            "env_type": "zoo",
+            "sequential_env": True,
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 1,  # for now only one agent
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
 
 
-# def test_qagent_in_savanna_gridworlds_parallel(
-#    tparams_hparams: Dict
-# ) -> None:
-#    full_params = tparams_hparams
-#    params_savanna_zoo = {
-#        "agent_id": "q_agent",
-#        "env": "savanna-safetygrid-parallel-v1",
-#        "env_entry_point": "aintelope.environments.savanna_safetygrid:SavannaGridworldParallelEnv",
-#        "env_type": "zoo",
-#        "env_params": {
-#            "num_iters": 40,  # duration of the game
-#            "map_min": 0,
-#            "map_max": 20,
-#            "render_map_max": 20,
-#            "amount_agents": 1,  # for now only one agent
-#            "amount_grass_patches": 2,
-#            "amount_water_holes": 0,
-#        },
-#        "agent_params": {},
-#    }
-#    full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
-#    run_episode(full_params=full_params)
+def test_qagent_in_savanna_zoo_parallel_with_death(
+    tparams_hparams: Dict,
+) -> None:
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        np.random.seed(index)
+
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-zoo-parallel-v2",
+            "env_entry_point": "aintelope.environments.savanna_zoo:SavannaZooParallelEnv",
+            "env_type": "zoo",
+            "sequential_env": True,
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 2,  # needed for death test
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+                "test_death": True,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
+
+
+def test_qagent_in_savanna_gridworlds_parallel(tparams_hparams: Dict) -> None:
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-safetygrid-parallel-v1",
+            "env_entry_point": "aintelope.environments.savanna_safetygrid:SavannaGridworldParallelEnv",
+            "env_type": "zoo",
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 1,  # for now only one agent
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+                "seed": index,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
+
+
+def test_qagent_in_savanna_gridworlds_parallel_with_death(
+    tparams_hparams: Dict,
+) -> None:
+    for index in range(
+        0, 10
+    ):  # construct the environment multiple times with different seeds
+        full_params = tparams_hparams
+        params_savanna_zoo = {
+            "agent_id": "q_agent",
+            "env": "savanna-safetygrid-parallel-v1",
+            "env_entry_point": "aintelope.environments.savanna_safetygrid:SavannaGridworldParallelEnv",
+            "env_type": "zoo",
+            "env_params": {
+                "num_iters": 40,  # duration of the game
+                "map_min": 0,
+                "map_max": 20,
+                "render_map_max": 20,
+                "amount_agents": 2,  # needed for death test
+                "amount_grass_patches": 2,
+                "amount_water_holes": 0,
+                "test_death": True,
+                "seed": index,
+            },
+            "agent_params": {},
+        }
+        full_params.hparams = OmegaConf.merge(full_params.hparams, params_savanna_zoo)
+        run_episode(full_params=full_params)
 
 
 if __name__ == "__main__" and os.name == "nt":  # detect debugging
