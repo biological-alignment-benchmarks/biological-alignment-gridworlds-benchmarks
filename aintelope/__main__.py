325923c7e6663487c840f4fbffecf812f08bf845
Roland Pihlakas <roland@simplify.ee>
2024-02-24 00:53:24 +0200

    Pass score dimensions to plotting

--- aintelope/__main__.py
+++ aintelope/__main__.py
@@ -31,17 +31,17 @@ def aintelope_main(cfg: DictConfig) -> None:
         logger.info(OmegaConf.to_yaml(experiment_cfg))
         run_experiment(experiment_cfg, score_dimensions)
 
-    analytics(experiment_cfg)
+    analytics(experiment_cfg, score_dimensions)
 
 
-def analytics(cfg):
+def analytics(cfg, score_dimensions):
     # normalise slashes in paths. This is not mandatory, but will be cleaner to debug
     log_dir = os.path.normpath(cfg.log_dir)
     events_dir = os.path.normpath(cfg.events_dir)
 
     savepath = os.path.join(log_dir, "plot.png")
     events = recording.read_events(log_dir, events_dir)
-    plotting.plot_performance(events, savepath)
+    plotting.plot_performance(events, score_dimensions, savepath)
 
 
 if __name__ == "__main__":
